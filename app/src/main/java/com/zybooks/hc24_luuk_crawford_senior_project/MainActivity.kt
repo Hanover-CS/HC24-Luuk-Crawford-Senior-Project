d4d506d4bb3dea3917206f7959477efe0189096e
idkChips <mrluuk@gmail.com>
2023-07-14 17:30:24 -0400

    button to swap activities working. updated manifest

--- app/src/main/java/com/zybooks/hc24_luuk_crawford_senior_project/MainActivity.kt
+++ app/src/main/java/com/zybooks/hc24_luuk_crawford_senior_project/MainActivity.kt
@@ -1,12 +1,21 @@
 package com.zybooks.hc24_luuk_crawford_senior_project
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        val buttonClick = findViewById<Button>(R.id.toSelectionActivity)
+        buttonClick.setOnClickListener {
+            val intent = Intent(this, SelectionActivity::class.java)
+            startActivity(intent)
+        }
     }
-    //TEST PUSH TO MAIN
+
 }
---
+++ app/src/main/java/com/zybooks/hc24_luuk_crawford_senior_project/SelectionActivity.kt
@@ -0,0 +1,17 @@
+package com.zybooks.hc24_luuk_crawford_senior_project
+
+import android.os.Bundle
+
+import androidx.appcompat.app.AppCompatActivity
+
+class SelectionActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_selection)
+
+
+
+    }
+
+
+}
