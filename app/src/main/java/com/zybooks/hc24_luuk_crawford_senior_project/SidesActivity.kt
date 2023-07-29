ae1ee44e490165350de3628db915c33a51dce03a
idkChips <mrluuk@gmail.com>
2023-07-28 22:30:32 -0400

    WIP on attempt (WIP WILL NOT RUN IN CURRENT STATE)

--- app/src/main/java/com/zybooks/hc24_luuk_crawford_senior_project/SidesActivity.kt
+++ app/src/main/java/com/zybooks/hc24_luuk_crawford_senior_project/SidesActivity.kt
@@ -4,13 +4,14 @@ import android.os.Bundle
 import android.os.PersistableBundle
 import androidx.appcompat.app.AppCompatActivity
 import com.zybooks.hc24_luuk_crawford_senior_project.databinding.ActivitySelectionBinding
+import com.zybooks.hc24_luuk_crawford_senior_project.databinding.ActivitySidesBinding
 
 class SidesActivity : AppCompatActivity(){
-    private lateinit var binding: ActivitySelectionBinding
+    private lateinit var binding: ActivitySidesBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivitySelectionBinding.inflate(layoutInflater)
+        binding = ActivitySidesBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         val name = intent.getStringExtra("name")
@@ -18,7 +19,7 @@ class SidesActivity : AppCompatActivity(){
         val side = intent.getStringExtra("side")
         val price = intent.getStringExtra("price")
 
-        binding.nameProfile.text = name
+
 
 
     }
