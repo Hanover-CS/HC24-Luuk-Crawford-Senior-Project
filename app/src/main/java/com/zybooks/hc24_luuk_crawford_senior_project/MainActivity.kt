4f8c5f392cff33b2b07b803a5e708d4cd72a60a6
idkChips <mrluuk@gmail.com>
2023-09-16 14:18:46 -0400

    basic start screen, now in jetpack compose. #6

--- app/src/main/java/com/zybooks/hc24_luuk_crawford_senior_project/MainActivity.kt
+++ app/src/main/java/com/zybooks/hc24_luuk_crawford_senior_project/MainActivity.kt
@@ -1,27 +1,43 @@
 package com.zybooks.hc24_luuk_crawford_senior_project
 
-import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
-import com.google.firebase.FirebaseApp
-import com.google.firebase.analytics.ktx.analytics
-import com.google.firebase.ktx.Firebase
 
 
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Text
+import androidx.compose.material.Button
 
 
 // ...
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            MessageCard("Android")
+
+            //MessageCard("Android")
+            WelcomeScreen()
+
         }
     }
 }
@@ -29,7 +45,65 @@ class MainActivity : ComponentActivity() {
 fun MessageCard(name: String) {
     Text(text = "Hello $name!")
 }
+@Composable
+fun WelcomeScreen() {
+    Column {
+        Spacer(modifier = Modifier.height(8.dp))
+        Text(
+            text = stringResource(id = R.string.collegeNameCaps),
+            fontSize = 35.sp,
+            fontWeight = FontWeight.Bold,
 
+            textAlign = TextAlign.Center,
+            color = colorResource(id = R.color.hanoverWebRed)
+        )
+        Text(text = stringResource(id = R.string.underground),fontSize = 35.sp,
+            fontWeight = FontWeight.Bold,
+            textAlign = TextAlign.Center,
+        )
+        ShowMenuButton()
+
+    }
+}
+
+@Composable
+fun ShowMenuButton() {
+    Column(
+        verticalArrangement = Arrangement.Bottom,
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = Modifier.fillMaxHeight()
+
+
+    ){
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(16.dp),
+            contentAlignment = Alignment.Center
+        ) {
+
+    Button(onClick = { /* do something on click */ },
+    shape = RoundedCornerShape(18.dp),
+    ) {
+        Text(
+            text = stringResource(id = R.string.beginOrder),
+            fontSize = 40.sp,
+            color = Color.White, textAlign = TextAlign.Center)
+    }}
+    }
+}
+
+
+
+/* https://developer.android.com/jetpack/compose/layouts/basics
+@Composable
+fun ArtistAvatar(artist: Artist) {
+    Box {
+        Image(bitmap = artist.image, contentDescription = "Artist image")
+        Icon(Icons.Filled.Check, contentDescription = "Check mark")
+    }
+}
+*/
 
 
 /*
