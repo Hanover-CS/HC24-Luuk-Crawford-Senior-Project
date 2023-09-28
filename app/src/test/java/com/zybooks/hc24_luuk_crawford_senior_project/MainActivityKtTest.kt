0767c879305501f1f43d763668c4baab3efe1d3b
idkChips <mrluuk@gmail.com>
2023-09-28 18:52:29 -0400

    setting up for tests #7 #15 #16 #17

---
+++ app/src/test/java/com/zybooks/hc24_luuk_crawford_senior_project/MainActivityKtTest.kt
@@ -0,0 +1,17 @@
+package com.zybooks.hc24_luuk_crawford_senior_project
+
+import junit.framework.TestCase.assertEquals
+import org.junit.Test
+
+import org.junit.Assert.*
+import org.junit.jupiter.api.Assertions.*
+
+internal class MainActivityKtTest{
+    @Test
+    fun test(){
+        assertEquals(3,3)
+    }
+
+
+}
+
