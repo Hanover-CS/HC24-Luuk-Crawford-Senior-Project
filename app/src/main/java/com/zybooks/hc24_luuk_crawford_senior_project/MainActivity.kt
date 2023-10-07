0a4a5d813be0f372bf05dfeaab970221944ed1b3
idkChips <mrluuk@gmail.com>
2023-10-07 12:49:35 -0400

    Testing buttons for menu
    #31
    #10
    #14

--- app/src/main/java/com/zybooks/hc24_luuk_crawford_senior_project/MainActivity.kt
+++ app/src/main/java/com/zybooks/hc24_luuk_crawford_senior_project/MainActivity.kt
@@ -19,6 +19,7 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Button
@@ -27,6 +28,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.layout
 import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.stringResource
@@ -245,14 +247,20 @@ fun MenuScreen(){
         items(myMenuList.size){index ->
             Row(
                 verticalAlignment = Alignment.CenterVertically,
-                modifier = Modifier.padding(10.dp).testTag("anItemExists")
+                modifier = Modifier
+                    .padding(10.dp)
+                    .testTag("anItemExists")
             ) {
-                Image(
-                    painter = rememberImagePainter(myMenuList[index].imageLink),
-                    contentDescription = stringResource(R.string.lostImage),
-                    modifier = Modifier.size(70.dp)
-                )
-                Text(text = myMenuList[index].name)
+                Button(onClick = { /*TODO*/ }, Modifier.fillMaxWidth()) {
+
+                    Image(
+                        painter = rememberImagePainter(myMenuList[index].imageLink),
+                        contentDescription = stringResource(R.string.lostImage),
+                        modifier = Modifier.size(70.dp)
+                    )
+                    Text(text = myMenuList[index].name)
+
+                }
             }
         }
     }
