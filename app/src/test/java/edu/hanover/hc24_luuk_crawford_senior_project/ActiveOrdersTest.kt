28828e5cdc2fda03fdaafaaa2d703594f0d0920f
idkChips <mrluuk@gmail.com>
2023-11-23 09:59:06 -0500

    data: ActiveOrdersTest complete was for active orders from database. Saving this but removing next push. #12

--- app/src/test/java/edu/hanover/hc24_luuk_crawford_senior_project/ActiveOrdersTest.kt
+++ app/src/test/java/edu/hanover/hc24_luuk_crawford_senior_project/ActiveOrdersTest.kt
@@ -5,13 +5,17 @@ import edu.hanover.hc24_luuk_crawford_senior_project.data.Customization
 import edu.hanover.hc24_luuk_crawford_senior_project.data.ItemStatus
 import edu.hanover.hc24_luuk_crawford_senior_project.data.UserOrder
 import org.junit.Assert.assertEquals
+import org.junit.Before
 import org.junit.Test
 
 class ActiveOrdersTest {
 
+    @Before
+    fun clearActiveOrdersBefore(){
+        ActiveOrders.clearActiveOrdersList()
+    }
     @Test
     fun addActiveOrderTest(){
-
         val exampleCustomization = Customization(mutableListOf("a"), mutableListOf("b"))
         val unixOrderStartTime = 1700746279L
 
@@ -20,5 +24,19 @@ class ActiveOrdersTest {
         assertEquals(mutableListOf<UserOrder>(),ActiveOrders.getActiveOrdersList())
         ActiveOrders.addActiveOrder(exampleOrder)
         assertEquals(mutableListOf<UserOrder>(exampleOrder), ActiveOrders.getActiveOrdersList())
+    }
+
+    @Test
+    fun clearActiveOrdersTest(){
+        val exampleCustomization = Customization(mutableListOf("a"), mutableListOf("b"))
+        val unixOrderStartTime = 1700746279L
+
+        val exampleOrder = UserOrder("user1", 5000, 7000,200, exampleCustomization,ItemStatus.inProgress,unixOrderStartTime,null)
+        ActiveOrders.addActiveOrder(exampleOrder)
+
+        assertEquals(mutableListOf<UserOrder>(exampleOrder), ActiveOrders.getActiveOrdersList())
+        ActiveOrders.clearActiveOrdersList()
+        assertEquals(mutableListOf<UserOrder>(),ActiveOrders.getActiveOrdersList())
+
     }
 }
