07e26402ee97e5ca852747f19f07d3101849eaa5
idkChips <mrluuk@gmail.com>
2023-11-23 09:38:50 -0500

    removing WIP viewing of active orders screen. Plan is to display it on projector. Not a priority. Submitting order now brings us back to welcome screen. #12 #38

--- app/src/main/java/edu/hanover/hc24_luuk_crawford_senior_project/composables/AppNavHost.kt
+++ app/src/main/java/edu/hanover/hc24_luuk_crawford_senior_project/composables/AppNavHost.kt
@@ -11,15 +11,12 @@ import androidx.navigation.compose.rememberNavController
 import edu.hanover.hc24_luuk_crawford_senior_project.data.currentOrder.CurrentOrderManager.getCurrentUserOrder
 import edu.hanover.hc24_luuk_crawford_senior_project.data.Destination
 import edu.hanover.hc24_luuk_crawford_senior_project.composables.screens.MenuScreen
-import edu.hanover.hc24_luuk_crawford_senior_project.composables.screens.OrdersScreen
-import edu.hanover.hc24_luuk_crawford_senior_project.composables.screens.OrdersScreenLoading
 import edu.hanover.hc24_luuk_crawford_senior_project.composables.screens.ToppingsScreen
 import edu.hanover.hc24_luuk_crawford_senior_project.composables.screens.WelcomeScreen
 
 
 /**
  * Nav host is the composable manager
- * set start location
  * handles which composable to load when triggered
  * It is set up like this to prevent giving navController itself to everything.
  * @param startDestination Which composable to start at (string). Defaults to WelcomeScreen.
@@ -36,27 +33,18 @@ fun AppNavHost(
         navController = navController,
         startDestination = startDestination
     ) {
-        //activated when button clicked.
         val onNavigateToMenuFunction =
             { navController.navigate(Destination.menuScreen.name) }
         val onNavigateToToppingsFunction =
             { navController.navigate(Destination.toppingsScreen.name) }
-        val onNavigateToOrdersLoadingScreen =
-            {Log.d(ContentValues.TAG, "nav to LOADING ORDERS")
-                navController.navigate(Destination.ordersScreenLoading.name)}
-        val onNavigateToOrdersScreen =
-            { Log.d(ContentValues.TAG, "nav to ORDERS")
-                navController.navigate(Destination.ordersScreen.name) }
-
+        val onNavigateToWelcomeScreen = {
+            navController.navigate(Destination.welcomeScreen.name)
+        }
 
         composable(Destination.welcomeScreen.name) { WelcomeScreen(onNavigateToMenuFunction) }
 
         composable(Destination.menuScreen.name) { MenuScreen(onNavigateToToppingsFunction) }
 
-        composable(Destination.toppingsScreen.name) { ToppingsScreen( onNavigateToOrdersLoadingScreen,getCurrentUserOrder()) }
-
-        composable(Destination.ordersScreenLoading.name){ OrdersScreenLoading(onNavigateToOrdersScreen)}
-
-        composable(Destination.ordersScreen.name){ OrdersScreen(onNavigateToOrdersLoadingScreen)}
+        composable(Destination.toppingsScreen.name) { ToppingsScreen( onNavigateToWelcomeScreen,getCurrentUserOrder()) }
     }
 }
