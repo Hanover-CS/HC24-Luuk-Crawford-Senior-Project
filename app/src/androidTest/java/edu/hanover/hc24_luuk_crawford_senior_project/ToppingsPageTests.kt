3a439eddfddcf1b65f9f83b447343ab489d4eb6d
idkChips <mrluuk@gmail.com>
2023-11-25 19:37:15 -0500

    checkboxes modify list correctly test #16

--- app/src/androidTest/java/edu/hanover/hc24_luuk_crawford_senior_project/ToppingsPageTests.kt
+++ app/src/androidTest/java/edu/hanover/hc24_luuk_crawford_senior_project/ToppingsPageTests.kt
@@ -31,7 +31,23 @@ internal class ToppingsPageTests {
     @Before
     fun itemsInMenu() {
         MenuData.clearMenuContents()
-        MenuData.addMenuItem(MenuItem("ExampleName",300, "Burger","https://i.imgur.com/BDa36Zp.jpeg"))
+        MenuData.addMenuItem(
+            MenuItem(
+                "ExampleName",
+                300,
+                "Burger",
+                "https://i.imgur.com/BDa36Zp.jpeg"
+            )
+        )
+        MenuData.addMenuItem(
+            MenuItem(
+                "ExampleTwo",
+                350,
+                "Burger",
+                "https://i.imgur.com/BDa36Zp.jpeg"
+            )
+        )
+
         val burgerSides = mutableListOf("Hand Cut Fries Test", "Mac-N-Cheese", "Tater Tots")
         val burgerToppings =
             mutableListOf("Lettuce Test", "Tomato", "Onion Test", "Pickle", "Cheese", "Bacon")
@@ -47,7 +63,7 @@ internal class ToppingsPageTests {
         composeTestRule.setContent {
             AppNavHost(Destination.menuScreen.name)
         }
-        navigateFromMenu("ExampleName")
+        navigateToToppingsWith("ExampleName")
         toppingsPageContentsExist()
     }
 
@@ -76,7 +92,7 @@ internal class ToppingsPageTests {
         checkbox.assertHasClickAction()
     }
 
-    private fun navigateFromMenu(itemToClick: String) {
+    private fun navigateToToppingsWith(itemToClick: String) {
         val itemExistsInList = composeTestRule.onNode(hasTestTag(itemToClick))
         itemExistsInList.assertIsDisplayed()
         itemExistsInList.assertHasClickAction()
@@ -85,16 +101,16 @@ internal class ToppingsPageTests {
     }
 
     @Test
-    fun menuTestCheckboxes() {
+    fun menuTestSingleCheckbox() {
         composeTestRule.setContent {
             AppNavHost(Destination.menuScreen.name)
         }
-        navigateFromMenu("ExampleName")
-
-        testToppingsCheckbox("Bacon", "toppings")
+        navigateToToppingsWith("ExampleName")
+        testCheckbox("Bacon", "toppings")
+        testCheckbox("Tater Tots", "sides")
     }
 
-    private fun testToppingsCheckbox(checkboxTestTag: String, customizationCategory: String) {
+    private fun testCheckbox(checkboxTestTag: String, customizationCategory: String) {
         val checkbox = composeTestRule.onNode(hasTestTag(checkboxTestTag))
         assert(receiveCustomCategoryList(customizationCategory).isEmpty())
 
@@ -118,12 +134,55 @@ internal class ToppingsPageTests {
             "toppings" -> {
                 customList = CurrentOrderManager.getCurrentUserOrder().customization.toppings
             }
+
             "sides" -> {
                 customList = CurrentOrderManager.getCurrentUserOrder().customization.sides
             }
-
         }
         return customList
+    }
+
+    @Test
+    fun checkboxesModifyListCorrectlyTest() {
+        composeTestRule.setContent {
+            AppNavHost(Destination.menuScreen.name)
+        }
+        navigateToToppingsWith("ExampleTwo")
+
+        val checkboxLettuce = composeTestRule.onNode(hasTestTag("Lettuce Test"))
+        val checkboxCheese = composeTestRule.onNode(hasTestTag("Cheese"))
+        val checkboxTomato = composeTestRule.onNode(hasTestTag("Tomato"))
+        val checkboxFries = composeTestRule.onNode(hasTestTag("Hand Cut Fries Test"))
+        val checkboxMacNCheese = composeTestRule.onNode(hasTestTag("Mac-N-Cheese"))
+
+        checkboxFries.performClick()
+        checkboxMacNCheese.performClick()
+        checkboxLettuce.performClick()
+        assertEquals(
+            mutableListOf("Hand Cut Fries Test", "Mac-N-Cheese"),
+            CurrentOrderManager.getCurrentUserOrder().customization.sides
+        )
+        checkboxCheese.performClick()
+        checkboxTomato.performClick()
+        checkboxFries.performClick()
+        assertEquals(mutableListOf("Lettuce Test", "Cheese", "Tomato"), CurrentOrderManager.getCurrentUserOrder().customization.toppings)
+        checkboxLettuce.performClick()
+        assertEquals(
+            mutableListOf("Mac-N-Cheese"),
+            CurrentOrderManager.getCurrentUserOrder().customization.sides
+        )
+        checkboxTomato.performClick()
+        checkboxFries.performClick()
+        assertEquals(
+            mutableListOf("Cheese"),
+            CurrentOrderManager.getCurrentUserOrder().customization.toppings
+        )
+        checkboxCheese.performClick()
+        checkboxFries.performClick()
+        assertEquals(
+            mutableListOf<String>(),
+            CurrentOrderManager.getCurrentUserOrder().customization.toppings
+        )
     }
 
 }
